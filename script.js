af30c52bfed4a2280301bee988297fc854ed7ac8
karenefereyan <karenefereyan@gmail.com>
2022-02-15 09:50:43 -0500

    Javascript functionality almost done

--- script.js
+++ script.js
@@ -9,11 +9,7 @@ const form = document.getElementById("form");
 
 form.addEventListener("submit", (e) => {
     //Prevent form from submitting
-
     e.preventDefault();
-
-    const errors = document.querySelector(" .form-field small");
-    console.log("This should output the small tag", errors)
 
 
     //validate form imputs
@@ -23,28 +19,23 @@ form.addEventListener("submit", (e) => {
     let isPasswordValid = checkPassword();
 
     //A valid form has all the inputs valid!!!
-
     let isFormValid = isFirstNameValid &&
     isLastNameValid && isEmailValid && isPasswordValid
 
     //submit valid form to server
     if(isFormValid){
-        alert("THANKS FOR SUBMITTING THE FORM!")
+        alert("THANKS FOR SUBMITTING THE FORM!");
     }
 });
 
 
 // Utility functions
 //1. Check that email is valid
+
 const isEmailValid = (email) => {
-    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(String(email).toLowerCase());
 };
-
-// function validateEmail(email){
-//     let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-// 	return re.test(String(email).toLowerCase());
-// }
 
 
 //2. Check that inputs are not empty
@@ -54,34 +45,41 @@ const isRequired = value => value === "" ? false :true;
 const showError = (input, message) => {
     const formField = input.parentElement;
 
-    input.placeholder = "";
-    formField.classList.add('emailedExample');
+   
     formField.classList.add('error');
     const errorMsg = formField.querySelector("small");
     errorMsg.textContent = message;
+    // input.placeholder = "";
 
-    formField.classList.remove("emailedExample");
+}
+
+const removeError =(input) => {
+    const formField = input.parentElement;
+    formField.classList.remove('error');
+    // const errorMsg = formField.querySelector('small');
+    // errorMsg.textContent = "";
 }
 
 //Input Field Validating Functions
-//1. First Name field validation
 
+//1. First Name field validation
 const checkFirstName = () => {
     let valid = false;
     const firstname = fname.value;
 
+    //If firstname input is empty
     if(!isRequired(firstname)){
         showError(fname, "First Name cannot be empty!");
         fname.focus();
     }
     else{
+        removeError(fname);
         valid = true;
     }
     return valid;
 }
 
 //2. Last Name field validation
-
 const checkLastName = () => {
     let valid = false;
 
@@ -92,12 +90,13 @@ const checkLastName = () => {
         lname.focus();
     }
     else{
+        removeError(lname);
         valid = true;
     }
     return valid;
 }
 
-//2. Email field validation
+//3. Email field validation
 const checkEmail = () => {
     let valid = false;
 
@@ -107,25 +106,28 @@ const checkEmail = () => {
         showError(email, "Please enter your email");
         email.focus()
     }
-    else if(!isEmailValid(email)){
-        showError(email, "Looks like this is not an email!")
+    else if(!isEmailValid(emailVal)){
+        showError(email, "Looks like this is not an email!");
     }
     else{
+        removeError(email);
         valid = true;
     }
     return valid;
 }
 
-//3. Password field validation
+//4. Password field validation
 const checkPassword = () => {
     let valid = false;
 
     const password1 = password.value;
 
     if(!isRequired(password1)){
-        showError(password, "Password cannot be empty!")
+        showError(password, "Password cannot be empty!");
+        password.focus();
     }
     else{
+        removeError(password);
         valid = true;
     }
     return valid;
